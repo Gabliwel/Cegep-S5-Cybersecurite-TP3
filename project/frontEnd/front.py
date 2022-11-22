486ff527a4e62f992e20873fdf6d8cbcaf1bb204
Gabriel Bertrand <gabriel_bertrand@hotmail.com>
2022-11-22 17:22:10 -0500

    method front end

--- project/frontEnd/front.py
+++ project/frontEnd/front.py
@@ -18,16 +18,69 @@ def build_home_page():
     else:
         data = {'message':'erreur!'}
     content_basic = str(data)
+    action_basic = 'action'
 
     return '''<html>
     <header></header>
     <body>
-        <p>Yes, un TP3 qui marche... je crois</p>
+        <h1>TP3</h1>
+        <h4>Réalisé par Gabriel bertrand et Keven Champagne</h4>
         <br>
-        BASIC DATA:
+        Message(s) basique:
 		<ul>
 			<li>''' + content_basic + '''</li>
 		</ul>
+        Message(s) d'action:
+		<ul>
+			<li>''' + action_basic + '''</li>
+		</ul>
+        <br>
+        <h5>Login</h5>
+        <form action="/login" method="POST">
+            <label>Nom d'utilisateur: </label>
+            <input type="text" name="username" required>
+            <label>Mot de passe: </label>
+            <input type="password" name="password" required>
+            <button type="submit">Login</button>
+        </form>
+        <br>
+        <h5>Recherche de compte</h5>
+        <form action="/research" method="POST">
+            <label>Terme de recherche: </label>
+            <input type="text" name="searchTerm" required>
+            <button type="submit">Rechercher</button>
+        </form>
+        <br>
+        <h5>Voir son compte</h5>
+        <a href="/viewAccount">Voir mon compte</a>
+        <br>
+        <h5>Transfert</h5>
+        <form action="/transfert" method="POST">
+            <label>Compte receveur: </label>
+            <input type="text" name="account" required>
+            <label>Montant: </label>
+            <input type="text" name="amount" required>
+            <button type="submit">Faire le transfer</button>
+        </form>
+        <br>
+        <h5>Voir la FAQ</h5>
+        <a href="/viewFaq">Voir la FAQ</a>
+        <br>
+        <h5>Ajouter un message à la FAQ</h5>
+        <form action="/addFaqMsg" method="POST">
+            <label>Message: </label>
+            <input type="text" name="message" required>
+            <button type="submit">Ajouter</button>
+        </form>
+        <br>
+        <h5>Créer un compte</h5>
+        <form action="/createAccount" method="POST">
+            <label>Nom: </label>
+            <input type="text" name="username" required>
+            <label>Mot de passe: </label>
+            <input type="password" name="password" required>
+            <button type="submit">Créer le compte</button>
+        </form>
     </body>
     </html>
     '''
@@ -35,6 +88,42 @@ def build_home_page():
 @app.route('/')
 def hello():
 	return build_home_page()
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    return build_home_page()
+
+@app.route('/research', methods=['POST'])
+def search():
+    searchTerm = request.form['searchTerm']
+    return build_home_page()
+
+@app.route('/viewAccount', methods=['GET'])
+def viewAccount():
+    return build_home_page()
+
+@app.route('/transfert', methods=['POST'])
+def transfer():
+    account = request.form['account']
+    amount = request.form['amount']
+    return build_home_page()
+
+@app.route('/viewFaq', methods=['GET'])
+def viewFaq():
+    return build_home_page()
+
+@app.route('/addFaqMsg', methods=['POST'])
+def addFaqMsg():
+    msg = request.form['message']
+    return build_home_page()
+
+@app.route('/createAccount', methods=['POST'])
+def createAccount():
+    account = request.form['username']
+    amount = request.form['password']
+    return build_home_page()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5556)
